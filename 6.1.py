2cd3171f2d9b549ef7fbbd1a664689a27f125d8b
William Sonnerklev <william.sonnerklev2@gmail.com>
2023-12-06 11:29:45 +0100

    day 6

---
+++ 6.1.py
@@ -0,0 +1,26 @@
+with open('inp.txt', 'r') as f:
+    l = f.read().split('\n')
+
+times = list(map(int, l[0].split()[1:]))
+dists = list(map(int, l[1].split()[1:]))
+
+p = 1
+
+for i in range(len(times)):
+    time = times[i]
+    dist = dists[i]
+
+    nWays = 0
+
+    for t in range(time):
+        d = t * (time - t)
+        if d > dist:
+            nWays += 1
+
+        # concave function (if t was continous), decreasing after we reach our maximum
+        # so we don't need to check the final values
+        elif d < dist and nWays > 0:
+            break
+    p *= nWays
+
+print(p)
